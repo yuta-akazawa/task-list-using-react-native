081c587fc1bd4f1d3e33e540bc55bf558fd5194c
yuta-akazawa <grief137@gmail.com>
2019-05-06 16:57:28 +0900

    add header in pofile screen

--- components/tabs/ProfileTab.js
+++ components/tabs/ProfileTab.js
@@ -1,35 +1,39 @@
 'use strict'
 
 import React, { Component } from 'react';
-import { 
+import {
     View,
     Text,
     StyleSheet,
- } from 'react-native';
-import {Icon} from 'native-base';
+} from 'react-native';
+import { Icon, Container, Header, Body, Title } from 'native-base';
 
 export default class ProfileTab extends Component {
     static navigationOptions = {
-        title:'A',
-        tabBarIcon: ({tintColor}) => (
-            <Icon name='person' style= {{ color: tintColor }} />
-          )
+        title: 'A',
+        tabBarIcon: ({ tintColor }) => (
+            <Icon name='person' style={{ color: tintColor }} />
+        )
     }
 
     render() {
-        return(
-            <View style={styles.container}>
-                <Text>ProfileTab</Text>
-            </View>
+        return (
+            <Container>
+                <Header>
+                    <Body>
+                        <Title>Task List</Title>
+                    </Body>
+                </Header>
+            </Container>
         );
     }
 }
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        backgroundColor:'#fff',
-        alignItems:'center',
-        justifyContent:'center',
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
     }
 });
